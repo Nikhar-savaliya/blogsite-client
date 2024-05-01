a5bb2b147b0c8954340c770da4e375f8cfa387fb
Nikhar S. <127284370+Nikhar-savaliya@users.noreply.github.com>
2024-05-01 10:08:04 +0530

    add browser routes

--- README.md
+++ README.md
@@ -3,3 +3,9 @@
 1. understand project structure.
 2. start
 
+## Rotes for frontend
+
+1. / -> layout
+   - index -> home
+   - signin -> signin form
+   - signup -> signup form
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,10 +1,55 @@
 import { createBrowserRouter } from "react-router-dom";
-import App from "../App";
+import Home from "../Home";
+import Layout from "@/Layout";
+import RegisterForm from "@/pages/auth/register";
+import LoginForm from "@/pages/auth/login";
+import SearchPage from "@/pages/search";
+import ProfilePage from "@/pages/profile";
+import NotFound from "@/pages/404NotFound";
+import EditorPage from "@/pages/editor";
+import BlogPage from "@/pages/blog";
 
 const browserRouter = createBrowserRouter([
   {
     path: "/",
-    element: <App />,
+    element: <Layout />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+      {
+        path: "register",
+        element: <RegisterForm />,
+      },
+      {
+        path: "login",
+        element: <LoginForm />,
+      },
+      {
+        path: "search/:title",
+        element: <SearchPage />,
+      },
+      {
+        path: "user/:id",
+        element: <ProfilePage />,
+      },
+      {
+        path: "blog/:id",
+        element: <BlogPage />,
+      },
+      {
+        path: "*",
+        element: <NotFound />,
+      },
+    ],
+  },
+  {
+    path: "/editor",
+    children: [
+      { index: true, element: <EditorPage /> },
+      { path: ":id", element: <EditorPage /> },
+    ],
   },
 ]);
 
